3c4c73ae35f4c971305c97663cf735f9909b70c0
volodymyrelert <volt.zt@gmail.com>
2022-10-19 09:21:32 +0200

    add api platform || add Product api && validation

---
+++ src/Entity/Product.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new Post(),
+        new GetCollection()
+    ]
+)]
+#[UniqueEntity('barcode')]
+class Product
+{
+    public const VAT_CLASS = [6, 22];
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[ApiProperty(identifier: false)]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 15, unique: true)]
+    #[ApiProperty(identifier: true)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 15)]
+    private ?string $barcode = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    #[Assert\PositiveOrZero]
+    private ?int $cost = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    #[Assert\Choice(choices: Product::VAT_CLASS, message: 'Choose a valid vat class.')]
+    private ?int $vat = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBarcode(): ?string
+    {
+        return $this->barcode;
+    }
+
+    public function setBarcode(string $barcode): self
+    {
+        $this->barcode = $barcode;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCost(): ?int
+    {
+        return $this->cost;
+    }
+
+    public function setCost(int $cost): self
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+
+    public function getVat(): ?int
+    {
+        return $this->vat;
+    }
+
+    public function setVat(int $vat): self
+    {
+        $this->vat = $vat;
+
+        return $this;
+    }
+}
